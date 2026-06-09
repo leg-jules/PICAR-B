66ec3134f7d7cdcf4ac0de62116419c2fc0051eb
Marco-Antonio Montenegro Loureiro <233918852+Marco9113@users.noreply.github.com>
2026-06-09 14:29:16 +0200

    Change distance unit from cm to mm

--- TestComposant/distance_ultrasons.py
+++ TestComposant/distance_ultrasons.py
@@ -6,10 +6,10 @@ Ec = 24
 sensor = DistanceSensor(echo=Ec, trigger=Tr,max_distance=2) # Distance de detection maximum 2m.
 
 def checkdist():
-    return (sensor.distance) *100 # Unité: cm
+    return (sensor.distance) *1000 # Unité: mm
 
 if __name__ == "__main__":
     while True:
         distance = checkdist() 
-        print("%.2f cm" %distance)
+        print("%.2f mm" %distance)
         sleep(0.05)
