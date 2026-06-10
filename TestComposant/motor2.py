d23e1396749c1cb4d850d4e1fcb32beaab864d9c
Aureprz <aurelien.91440@gmail.com>
2026-06-10 15:25:37 +0200

    Enhance Motor class with type hints and detailed docstrings for methods

--- TestComposant/motor2.py
+++ TestComposant/motor2.py
@@ -28,7 +28,7 @@ def map(x,in_min,in_max,out_min,out_max):
 	return (x - in_min)/(in_max - in_min) *(out_max - out_min) + out_min
 
 class Motor:
-	def __init__(self):
+	def __init__(self) -> None:
 			# Initialisation matérielle
 			self.i2c = busio.I2C(SCL, SDA)
 			self.pwm_motor = PCA9685(self.i2c, address=0x5f)
@@ -42,13 +42,29 @@ class Motor:
 			self.nb_motors = len(self.motors)
 
 
-	def setSpeed(self, new_direction, new_speed, pente= 100, channel=1):
+	def setSpeed(self, new_direction, new_speed, pente=1, channel=1) -> None:
+		"""
+		Définit la vitesse et la direction d'un moteur
+		:param new_direction (int): 1 pour avancer, -1 pour reculer
+		:param new_speed (int): Vitesse cible entre 0 et 100
+		:param pente (float): (default=1) Plus la pente est faible, plus la transition est lente EX: 0.5 pour une transition en 2 secondes, 1 pour une transition en 1 seconde
+		:param channel (int): Numéro du moteur (default=1) entre 1 et le nombre de moteurs disponibles
+		"""
+
 		# Sécurisation des limites de vitesse entre 0 et 100
 		new_speed = max(0, min(new_speed, 100))
 		self.transition(channel, new_speed, new_direction, pente)
 
 
-	def transition(self, channel, target_speed, target_direction, pente):
+	def transition(self, channel, target_speed, target_direction, pente) -> None:
+		""""
+		Effectue une transition progressive de la vitesse actuelle à la vitesse cible en fonction de la pente spécifiée.
+		:param channel (int): Numéro du moteur entre 1 et le nombre de moteurs disponibles
+		:param target_speed (int): Vitesse cible entre 0 et 100
+		:param target_direction (int): Direction cible (1 pour avancer, -1 pour reculer)
+		:param pente (float): (default=1) Plus la pente est faible, plus la transition est lente EX: 0.5 pour une transition en 2 secondes, 1 pour une transition en 1 seconde
+		"""
+
 		# Vérification de la validité du canal
 		if self.checkMotor(channel-1):
 			return
@@ -78,16 +94,30 @@ class Motor:
 		self.current_speed[channel-1] = target_speed
 		self.current_direction[channel-1] = target_direction
 
+
 	# Arrêt progressif du moteur
-	def motorStop(self, channel=None, pente=1):
+	def motorStop(self, channel=None, pente=1) -> None:
+		"""
+		Arrête progressivement le moteur en réduisant la vitesse à zéro selon la pente spécifiée.
+		:param channel (int): Numéro du moteur à arrêter (default=None pour tous les moteurs)
+		:param pente (float): Plus la pente est faible, plus l'arrêt est lent (default=1) EX: 0.5 pour un arrêt en 2 secondes, 1 pour un arrêt en 1 seconde
+		"""
+
 		if channel is None:
 			for i in range(len(self.motors)):
 				self.transition(i+1, 0, 1, pente)
 		else:
 			self.transition(channel, 0, 1, pente)
 
+
 	# Récupération de la vitesse actuelle
-	def getSpeed(self, channel = None):
+	def getSpeed(self, channel = None) -> int:
+		"""
+		Récupère la vitesse actuelle du moteur spécifié ou de tous les moteurs si aucun canal n'est précisé.
+		:param channel (int): Numéro du moteur à interroger (default=None pour tous les moteurs)
+		:return (int or list of int): Vitesse actuelle du moteur ou liste de vitesses pour tous les moteurs
+		"""
+
 		if channel is None:
 			return self.current_speed
 		else:
@@ -96,8 +126,15 @@ class Motor:
 				return
 			return self.current_speed[channel-1]
 	
+
 	# Récupération de la direction actuelle
-	def getDirection(self, channel = None):
+	def getDirection(self, channel = None) -> int:
+		"""
+		Récupère la direction actuelle du moteur spécifié ou de tous les moteurs si aucun canal n'est précisé.
+		:param channel (int): Numéro du moteur à interroger (default=None pour tous les moteurs)
+		:return (int or list of int): Direction actuelle du moteur ou liste de directions pour tous les moteurs
+		"""
+
 		if channel is None:
 			return self.current_direction
 		else:
@@ -106,22 +143,40 @@ class Motor:
 				return
 			return self.current_direction[channel-1]
 	
+
 	# Mise à jour de la vitesse et de la direction pour tous les moteurs
-	def setAllSpeeds(self, new_direction, new_speed, pente=100):
+	def setAllSpeeds(self, new_direction, new_speed, pente=100) -> None:
+		"""
+		Mets à jour la vitesse et la direction de tous les moteurs simultanément.
+		:param new_direction (int): 1 pour avancer, -1 pour reculer
+		:param new_speed (int): Vitesse cible entre 0 et 100
+		:param pente (float): (default=1) Plus la pente est faible, plus la transition est lente EX: 0.5 pour une transition en 2 secondes, 1 pour une transition en 1 seconde
+		"""
 		for i in range(len(self.motors)):
 			self.setSpeed(new_direction, new_speed, pente, channel=i+1)
 	
 
 	# Arrêt brutal de tous les moteurs
-	def destroy(self):
+	def destroy(self) -> None:
+		"""
+		Arrête immédiatement tous les moteurs et libère les ressources matérielles.
+		"""
 		self.motorStop(10)
 		self.pwm_motor.deinit()
 		self.i2c.deinit()
 	
-	def checkMotor(self, channel):
+	def checkMotor(self, channel) -> bool:
+		"""
+		Vérifie si le numéro de moteur spécifié est valide.
+		:param channel (int): Numéro du moteur à vérifier
+		:return (bool): True si le numéro de moteur est invalide, False sinon
+		"""
 		return not (0 <= channel < self.nb_motors)
 	
-	def __del__(self):
+	def __del__(self) -> None:
+		"""
+		Nettoie les ressources lors de la destruction de l'objet Motor.
+		"""
 		self.destroy()
 	
 	
