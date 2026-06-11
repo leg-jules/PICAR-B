0dc0b0a3d21a57c0fba7cd4319df8e75c9a5dbd4
leg-jules <149069485+leg-jules@users.noreply.github.com>
2026-06-11 10:12:06 +0200

    Improve comments for clarity in Tach6_line_following.py

--- TestComposant/Tach6_line_following.py
+++ TestComposant/Tach6_line_following.py
@@ -2,27 +2,27 @@ import time
 from gpiozero import InputDevice
 
 line_pin_left = 22
-line_pin_middle = 27 #attribution des pins 
+line_pin_middle = 27 # Definition des GPIO utilises par les trois capteurs de ligne
 line_pin_right = 17
 
 left = InputDevice(pin=line_pin_left)
-middle = InputDevice(pin=line_pin_middle) # attribue les capteurs au sens 
+middle = InputDevice(pin=line_pin_middle) # Creation des objets correspondant aux capteurs
 right = InputDevice(pin=line_pin_right)
 
 def run():
     status_left = left.value
-    status_middle = middle.value
+    status_middle = middle.value # lecture de l'etat de chaque capteur
     status_right = right.value
 
     print(
         'left: %d   middle: %d   right: %d'
-        % (status_left, status_middle, status_right) # affiche si le capteur reconnait une ligne
+        % (status_left, status_middle, status_right) # affiche si le capteur reconnait une ligne 
     )
 
 if __name__ == '__main__': 
     try:
-        while True:
+        while True: # boucle infini de lecture
             run()
-            time.sleep(0.3)
+            time.sleep(0.3)# latence entre capatation
     except KeyboardInterrupt:
         pass
